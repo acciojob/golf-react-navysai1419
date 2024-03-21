f5b9039f922dc9157f43ccd38ee7bf37ede9fd82
Navya <88023981+navysai1419@users.noreply.github.com>
2024-03-21 12:55:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,55 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      ballPosition: 0,
     };
+  }
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
+  buttonClickHandler = () => {
+    this.setState({ renderBall: true });
+  };
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event) => {
+    if (event.key === 'ArrowRight' && this.state.renderBall) {
+      this.setState((prevState) => ({
+        ballPosition: prevState.ballPosition + 5,
+      }));
     }
+  };
+
+  renderChoice = () => {
+    if (this.state.renderBall) {
+      return (
+        <div
+          className="ball"
+          style={{ left: `${this.state.ballPosition}px` }}
+        ></div>
+      );
+    } else {
+      return (
+        <button className="start" onClick={this.buttonClickHandler}>
+          Start Game
+        </button>
+      );
+    }
+  };
+
+  render() {
+    return <div className="container">{this.renderChoice()}</div>;
+  }
 }
-
 
 export default App;
